f8e99eb6e21b8901f35b75bc1b0fa82e41f730ef
VarshaKaranth <75885234+VarshaKaranth@users.noreply.github.com>
2025-01-23 14:53:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,67 +1,12 @@
-provider "aws" { 
-  region = "us-west-2" 
-}
-resource "aws_s3_bucket" "ci_cd_bucket" {
-    bucket = "gha-terraform"
-}
-resource "aws_iam_role" "github_actions_role" {
-  name = "github_actions_role"
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:CreateBucket",
-                "s3:DeleteBucket",
-                "s3:ListBucket",
-                "s3:GetBucketLocation",
-                "s3:PutBucketPolicy",
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:DeleteObject",
-                "s3:ListObjectsV2"
-            ],
-            "Resource": [
-                "arn:aws:s3:::gha-terraform",
-                "arn:aws:s3:::gha-terraform/*"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:CreateRole",
-                "iam:DeleteRole",
-                "iam:AttachRolePolicy",
-                "iam:DetachRolePolicy",
-                "iam:PutRolePolicy",
-                "iam:DeleteRolePolicy",
-                "iam:GetRole",
-                "iam:ListRolePolicies",
-                "iam:ListAttachedRolePolicies",
-                "iam:PassRole"
-            ],
-            "Resource": "arn:aws:iam::202533515928:role/GitLabRole"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:CreatePolicy",
-                "iam:DeletePolicy",
-                "iam:GetPolicy",
-                "iam:ListPolicies",
-                "iam:AttachUserPolicy",
-                "iam:DetachUserPolicy"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-EOF  
+provider "aws" {
+  region = "us-west-2"
 }
 
-resource "aws_iam_role_policy_attachment" "attach_policy" {
-    role = aws_iam_role.github_actions_role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+resource "aws_instance" "example" {
+  ami           = "ami-093a4ad9a8cc370f4" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
 }
