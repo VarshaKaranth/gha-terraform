f0fec262af479a7602b8184cfe3145be4fac963a
VarshaKaranth <75885234+VarshaKaranth@users.noreply.github.com>
2025-01-23 15:06:19 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ async function run() {
     try {
         const exampleInput = core.getInput('exampleInput');
         core.info(`Example Input: ${exampleInput}`);
-        core.setOutput('exampleOutput', 'Hello world');
     } catch (error) {
         core.setFailed(error.message);
     }
